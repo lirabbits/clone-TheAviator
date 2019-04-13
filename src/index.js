dfa5844915169595a271c56cc01a290331495162
noramage <noramageca@gmail.com>
2019-04-13 19:54:49 +0900

    add color variable

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import "./styles.scss";
+
+// Color Palette
+const colors = {
+  red: 0xf25e46,
+  white: 0xd8d0d1,
+  brown: 0x59332e,
+  pink: 0xf5986e,
+  brownDark: 0x23190f,
+  blue: 0x68c3c0
+};
